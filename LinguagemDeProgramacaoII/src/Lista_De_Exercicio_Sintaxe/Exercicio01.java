7dd69c193b882a1ec3acf263a36b9bca3dd8d893
gabrielcaproni <gabrielcaproni@gmail.com>
2023-02-09 10:50:41 -0300

    Lista Sintaxe

---
+++ LinguagemDeProgramacaoII/src/Lista_De_Exercicio_Sintaxe/Exercicio01.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lista_De_Exercicio_Sintaxe;
+
+/**
+ *
+ * @author Gabriel Pegoraro
+ */
+public class Exercicio01 {
+    public static void main(String[] args) {
+        int i = 0;
+        for(i = 330; i <= 450; i++){
+            System.out.println(i);
+        }
+    }
+}
---
+++ LinguagemDeProgramacaoII/src/Lista_De_Exercicio_Sintaxe/Exercicio03.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lista_De_Exercicio_Sintaxe;
+
+/**
+ *
+ * @author Gabriel Pegoraro
+ */
+public class Exercicio03 {
+    public static void main(String[] args) {
+        int i, mult = 0;
+        for(i = 0; i < 100; i += 5){
+            System.out.println(i);
+        }
+            
+        System.out.println("");
+    }
+}
