b8a8c61ef61eb84bbdc6a6804ac9858cac99cda9
gabrielcaproni <gabrielcaproni09@gmail.com>
2023-02-15 08:29:19 -0300

    Aula Túlio 15-02-23

---
+++ Projeto Tulio/GabrielPegoraro/src/banco/Conta.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package banco;
+
+/**
+ *
+ * @author Gabriel Pegoraro
+ */
+public class Conta {
+         
+        String cliente;
+        double saldo = 0;
+        double limite;
+    
+        public void deposita(double saldo){ // método 
+            this.saldo = this.saldo + saldo;
+            System.out.println("Depósito efetuado com sucesso !");
+        }
+        
+        
+        public boolean saca(double saldo) {
+            
+            if(this.saldo >= saldo){
+            this.saldo = this.saldo - saldo;
+            
+            System.out.println("Saque efetuado com sucesso !");
+            return true;
+            
+            }else{
+                System.out.println("Saldo Insuficiente para saque !");
+                return false;
+            }
+            
+    }
+               public boolean transfere(Conta conta, double valor){
+                   
+                   if(this.saldo < valor){
+                       return false;
+                       
+                   }else{
+                       
+                      this.saca(valor);
+                      conta.deposita(valor);
+                      return true;
+                   }
+                                   
+               }
+                
+              
+}
---
+++ Projeto Tulio/GabrielPegoraro/src/banco/Programa.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package banco;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Gabriel Pegoraro
+ */
+public class Programa {
+    public static void main(String[] args) {
+        
+        Scanner entrada = new Scanner(System.in);
+        
+        Conta minhaConta = new Conta(); //criando um objeto 
+        
+        minhaConta.cliente = "Gabriel Caproni Pegoraro";
+        System.out.println(minhaConta); 
+        
+        System.out.println("Digite um valor para depósito: ");
+        minhaConta.deposita(entrada.nextDouble());
+        System.out.println("Saldo minha conta: " + minhaConta.saldo);
+       
+        System.out.println("Digite um valor para saque: ");  
+        minhaConta.saca(entrada.nextDouble());
+        
+        System.out.println("Nome do cliente: " + minhaConta.cliente);
+        System.out.println("Saldo após saque: " + minhaConta.saldo);
+        
+        Conta outraConta = new Conta(); //criando um objeto
+                
+        System.out.println(outraConta);
+        System.out.println("Informe um valor para transferir: ");
+        boolean transfere = minhaConta.transfere(outraConta, entrada.nextDouble());
+        if(transfere){
+            System.out.println("Tranferencia realizada com sucesso !");
+            System.out.println("Saldo da conta debitada: R$ " + minhaConta.saldo);
+            System.out.println("Saldo da conta creditada: R$ " + outraConta.saldo);
+            
+        }else{
+            System.out.println("Nao foi possivel realizar a transferencia, pois o saldo e insuficiente !");
+        }
+    }
+}
