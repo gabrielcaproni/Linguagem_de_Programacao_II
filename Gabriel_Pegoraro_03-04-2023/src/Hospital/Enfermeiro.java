48204c19cd6653bc04602187a3bb2afb20f78d51
gabrielcaproni <gabrielcaproni09@gmail.com>
2023-04-03 20:58:46 -0300

    Atividade em progresso ...

---
+++ Gabriel_Pegoraro_03-04-2023/src/Hospital/Enfermeiro.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Hospital;
+
+/**
+ *
+ * @author Gabriel Pegoraro
+ */
+public class Enfermeiro {
+    private String nome;
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+}
---
+++ Gabriel_Pegoraro_03-04-2023/src/Hospital/Paciente.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Hospital;
+
+/**
+ *
+ * @author Gabriel Pegoraro
+ */
+public class Paciente {
+    private String nome;
+    private double totalPaciente;
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the totalPaciente
+     */
+    public double getTotalPaciente() {
+        return totalPaciente;
+    }
+
+    /**
+     * @param totalPaciente the totalPaciente to set
+     */
+    public void setTotalPaciente(double totalPaciente) {
+        this.totalPaciente = totalPaciente;
+    }
+    
+}
